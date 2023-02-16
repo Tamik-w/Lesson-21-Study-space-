aa2c115eef96fa1304b3737414b700c9c5646379
Tamurov_Dmitrii <dtamurov19@gmail.com>
2023-02-16 11:18:50 +0300

    task 2

--- Task 2/JS/app.js
+++ Task 2/JS/app.js
@@ -2,20 +2,21 @@ let seleries = {
     frontend: 12000,
     backend: 10000,
     designer: 8000,
-};
-function dayPay(){
-    alert('We must pay salary on Tuesday!')
+    dayPay(){
+        alert('We must pay salary on Tuesday!')
+    },
+
+    total(){
+        const values = Object.values(this).filter(item => typeof item === "number");
+        let sum = values.reduce((pSum, a) => pSum + a, 0);
+        console.log(sum);
+    },
 };
 
-function total(){
-    const values = Object.values(seleries);
-    let sum = values.reduce((pSum, a) => pSum + a, 0);
-    console.log(sum);
-};
 
-dayPay();
-total();
+seleries.dayPay();
+seleries.total();
 
 seleries.manager = 5000;
 
-total();
+seleries.total();
